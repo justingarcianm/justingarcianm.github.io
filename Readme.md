93b52d28b7e5b6475232c1650c5bf97d1760287d
justin garcia <justinframedev@gmail.com>
2018-12-15 13:58:47 -0700

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+** Welcome to my New Portfolio!
+
+* languages used:
+    -html
+    -css3
+    -javascript
+* with help from these cdn's:
+    -w3schools
+    -font awesome
---
+++ js/main.js
@@ -0,0 +1,19 @@
+var showArrow = document.querySelector("#showArrow");
+var arrowContent = document.querySelector('#arrowContent');
+var hideArrow = document.querySelector('#hideArrow');
+
+function showContent() {
+    arrowContent.style.opacity = "1";
+    arrowContent.classList.add("w3-animate-opacity");
+    showArrow.style.display = "none";
+
+}
+
+function hideContent() {
+    arrowContent.style.opacity = "0";
+    showArrow.style.display = "block";
+    arrowContent.classList.remove("w3-animate-opacity");
+}
+showArrow.addEventListener("click", showContent, false);
+hideArrow.addEventListener("click", hideContent, false);
+
