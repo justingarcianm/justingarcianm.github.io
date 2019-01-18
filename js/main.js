37ea3d456b8ae768a1b7ee3f6da83ee95a4dad68
justin garcia <justinframedev@gmail.com>
2019-01-18 13:07:11 -0700

    restart slider functions

--- js/main.js
+++ js/main.js
@@ -18,3 +18,41 @@ function hideContent() {
 showArrow.addEventListener("click", showContent, false);
 hideArrow.addEventListener("click", hideContent, false);
 
+// toggle slides for each project
+
+var proj1 = document.getElementById("proj-1"),
+    proj2 = document.getElementById("proj-2"),
+    proj3 = document.getElementById("proj-3"),
+    proj4 = document.getElementById("proj-4"),
+    proj5 = document.getElementById("proj-5"),
+    proj6 = document.getElementById("proj-6");
+
+var projectTitle = document.querySelectorAll(".project-title")
+
+function showProject(event) {
+    if (event.target === proj1) {
+        test("proj1");
+    } else if (event.target === proj2) {
+        test("proj2");
+    } else if (event.target === proj3) {
+        test("proj3");
+    } else if (event.target === proj4) {
+        test("proj4");
+    } else if (event.target === proj5) {
+        test("proj5");
+    } else if (event.target === proj6) {
+        test("proj6");
+    } else {
+        console.log("not a proj")
+    }
+}
+
+function test(test) {
+    console.log(test)
+}
+
+(function () {
+    for (let i = 0; i < projectTitle.length; i++) {
+        projectTitle[i].addEventListener("click", showProject);
+    }
+})();
